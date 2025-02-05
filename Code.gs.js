ed8760f3c34c6b73b014f9afd91825e61fa3d10e
Ray <ray@redment.com>
2025-02-04 20:35:50 -0800

    update v1: in sheet data parse, render, & formatting

--- Code.gs.js
+++ Code.gs.js
@@ -1,9 +1,4 @@
-/*
- * workout-manager | Workout Manager Apps Script (for Google Sheets)
- * Copyright (C) 2025 Ray Mentose (https://github.com/ryt)
- */
-
-// --- Start: Remote Data Functions --- //
+// --- Start: Remote Data Helpers --- //
 
 function parseCSV(csvString, ignoreHeaders) {
   // --- simple csv parser modified from: modjeska.us/csv-google-sheets-basic-auth --- //
@@ -36,13 +31,17 @@ function embedRemoteData(url, base64pw, randStr, csvParse, ignoreHeaders) {
   return resp.getContentText();
 }
 
-// --- End: Remote Data Functions --- //
+// --- End: Remote Data Helpers --- //
 
 
 // === Workout Manager Main === //
 
 
-// -- Start: Helper Functions (splitAtLast, convertDate, alrt) -- //
+// -- Helpers: alrt(), splitAtLast(), convertDate() -- //
+
+function alrt(msg) {
+  SpreadsheetApp.getUi().alert(msg);
+}
 
 function splitAtLast(str, char) { /* via googai */
   const lastIndex = str.lastIndexOf(char);
@@ -59,25 +58,102 @@ function convertDate(dateString) { /* via googai */
   return `${month} ${day}`;
 }
 
-function alrt(msg) {
-  SpreadsheetApp.getUi().alert(msg);
+function currentTime(format = 'short') {
+  // --- update time set: current date & time in format: 1/1 12:30:06p --- //
+  const date = new Date()
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const year = date.getFullYear().toString().slice(-2);
+  const hours = date.getHours();
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  const period = hours >= 12 ? 'p' : 'a';
+  const formattedHours = hours % 12 || 12;
+  let output = '';
+  if ( format == 'short' ) {
+    output = `${month}/${day} ${formattedHours}:${minutes}${period}`;
+  } else if ( format == 'long' ) {
+    output = `${month}/${day}/${year} ${formattedHours}:${minutes}:${seconds}${period}`;
+  }
+  return output;
 }
 
-// -- End: Helper Functions -- //
+// -- End: Helpers -- //
+
+// -- Sheet Cell Helpers: convertCellLocationTags(), formatCellsList, formatCellsAdd(), formatCellsApply(), renderCSVList() -- //
+
+function convertCellLocationTags(cell, r, c) {
+  // --- receives a cell value string and converts cell location tags (e.g. $cellref, $colref) to A1 references --- //
+  var alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''); // only 26 columns supported
+  // $cellref(r, c)
+  cell = cell.replace(/\$cellref\(([\d-]+),([\d-]+)\)/g, (match, row, col) => {
+    // r[0] = row 1
+    // c[0] = col A
+    ref_row = parseInt(row) + 1;
+    ref_col = parseInt(col);
+    return `${alpha[c+ref_col]}${r+ref_row}`;
+  });
+  // $colref(c) = A, B, C, ...
+  cell = cell.replace(/\$colref\(([\d-]+)\)/g, (match, col) => {
+    // c[0] = col A
+    ref_col = parseInt(col);
+    return `${alpha[c+ref_col]}`;
+  });
+  return cell;
+}
+
+let formatCellsList = [];
+
+function formatCellsAdd(format, rstart, cstart, rend, cend) {
+  // --- add formatting locations (row start, col start, row end, col end) to list --- //
+  var alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''); // only 26 columns supported
+  formatCellsList.push({
+    'format': format,
+    'range' : `${alpha[cstart]}${rstart}:${alpha[cend]}${rend}`
+  }); // convert rc to A1 Notation
+}
+
+function formatCellsApply() {
+  // --- apply formatting to added format cell locations --- //
+  if ( formatCellsList.length ) {
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+    for ( var i = 0; i < formatCellsList.length; i++ ) {
+      var format = formatCellsList[i].format;
+      var range = sheet.getRange(formatCellsList[i].range);
+      if ( format == 'bold' ) {
+        range.setFontWeight('bold');
+      }
+      if ( format == 'blackHeader' ) {
+        range.setFontWeight('bold');
+        range.setBackground('black');
+        range.setFontColor('white');
+      }
+      if ( format == 'lightHeader' ) {
+        range.setFontWeight('bold');
+        range.setBackground('#efefef');
+      }
+    }
+  }
+}
+
+function renderCSVList(rows, colLimit = 8) {
+  // --- receives a 2 dimensional list (csv style) (e.g. [['', ''], ['', '']]) & renders to spreadsheet --- //
+  var rowCount = rows.length; // number of rows in the CSV
+  var colCount = Math.min(colLimit, rows[0].length); // number of columns with limit
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var range = sheet.getRange(1, 1, rowCount, colCount); // set values in the range A1:colLimit(dynamic)
+  sheet.getRange(1, 1, rowCount + 100, colCount).clearContent(); // clear values (+100 of last row)
+  sheet.getRange(1, 1, rowCount + 100, colCount).clearFormat();  // clear formatting (+100 of last row)
+  range.setValues(rows);
+  formatCellsApply();
+}
+
+// -- End: Sheet Cell Helpers -- //
+
+// -- Workout Data Raw Format Parser & CSV Generator Main: workoutDataparser() -- //
 
 function workoutDataParser(input) {
   // --- receives a single workout data entry (v1 or v2) and converts it into object --- //
-  /*
-    --- v1 data example ---
-    workouts 4x rst:30s
-    - 5x pull up, 5 3 2 2
-    - db ovh press 2x20lb, 20 12 12 10
-
-    --- v2 data example ---
-    workout, 1/11 7p, (sw1: pull ups, 1 biceps), (garmin=id_or_url, key=val)
-    . 4, pull up, body, 7 4 3 4, 45s
-    . 4, hammer, 2x25lb, 16 12 8 10, 30s
-  */
   let workout_data = { entry_type: '', workout_details: {}, workout_list: [] };
 
   // parser start
@@ -196,7 +272,6 @@ function workoutDataParser(input) {
           global_set_count: global_set_count, // global set count
           global_rest_time: global_rest_time, // global rest time
         }
-
       }
 
       // body rows parser start
@@ -222,7 +297,6 @@ function workoutDataParser(input) {
         }
       }
       // body rows parser end
-
     }
     // end section for v2 workouts format
 
@@ -234,31 +308,8 @@ function workoutDataParser(input) {
       workout_data['workout_details']['name'] = week_val;
     }
     // end section for optional headers
-
   }
-
   return workout_data;
-
-}
-
-function convertCellLocationTags(cell, r, c) {
-  // --- receives a cell value string and converts cell location tags (e.g. $cellref, $colref) to A1 references --- //
-  var alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''); // only 26 columns supported
-  // $cellref(r, c)
-  cell = cell.replace(/\$cellref\(([\d-]+),([\d-]+)\)/g, (match, row, col) => {
-    // r[0] = row 1
-    // c[0] = col A
-    ref_row = parseInt(row) + 1;
-    ref_col = parseInt(col);
-    return `${alpha[c+ref_col]}${r+ref_row}`;
-  });
-  // $colref(c) = A, B, C, ...
-  cell = cell.replace(/\$colref\(([\d-]+)\)/g, (match, col) => {
-    // c[0] = col A
-    ref_col = parseInt(col);
-    return `${alpha[c+ref_col]}`;
-  });
-  return cell;
 }
 
 function parseWorkouts(filedata) {
@@ -267,6 +318,7 @@ function parseWorkouts(filedata) {
   filedata = filedata.split(/\n\s*\n/).filter(item=>item);
   if ( filedata.length ) {
     final.push(['Date','Sets','Exercise','Weights','Reps','Rep Avg','Rep Tot','Hr/Rest Set:Ex']);
+    formatCellsAdd('blackHeader', final.length, 0, final.length, 7); // set blackHeader for main header row
     for ( var i = 0; i < filedata.length; i++ ) {
       if ( filedata[i] ) {
         let workout = workoutDataParser(filedata[i].trim());
@@ -281,6 +333,7 @@ function parseWorkouts(filedata) {
               `)` +
             `)`;
           final.push([convertDate(wkdetails['date']), calc_tot_down, wkdetails['name'], '', '', '', calc_tot_down, wkdetails['hour']]);
+          formatCellsAdd('bold', final.length, 0, final.length, 7); // set bold for 'date' rows
           for ( var j = 0; j < wklist.length; j++ ) {
             let wkeach = wklist[j];
             let calc_rep_avg = '=iferror(round(average(split($cellref(0,-1)," ")), 2))';
@@ -306,6 +359,7 @@ function parseWorkouts(filedata) {
               `)`+
             `)/2`;
           final.push([workout['workout_details']['name'], calc_sum_week_sets, '', '', '', '', calc_sum_week_reps, '']);
+          formatCellsAdd('lightHeader', final.length, 0, final.length, 7); // set lightHeader for 'week' rows
         }
       }
     }
@@ -317,29 +371,83 @@ function parseWorkouts(filedata) {
     }
   }
   
-  var rows = final;
-  var rowCount = rows.length; // number of rows in the CSV
-  var colCount = Math.min(8, rows[0].length); // limit to 8 columns (A:H)
+  return final;
 
-  // alrt(JSON.stringify(final, '', 2));
-
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-
-  var range = sheet.getRange(1, 1, rowCount, colCount); // set values in the range A1:H(dynamic)
-  sheet.getRange(1, 1, rowCount + 100, colCount).clearContent(); // clear rows of upto +100 of last row
-  range.setValues(rows);
-  return false;
-  
 }
+
+// --- App Start: populateWorkouts(), onEdit(e) -- //
 
 function populateWorkouts() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  parseWorkouts(embedRemoteData(
-    sheet.getRange("J1").getValue(), 
-    "AUTH_HASH"),
-    sheet.getRange("J2").getValue(),
-  ); 
+  // --- main function that calls everything --- //
+  const cell_data    = 'J1';
+  const cell_remote  = 'J2';
+  const cell_udate   = 'L1';
+  const data_columns = 8; // (A:H)
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+
+  let remote_options = { 'use' : 'false', 'url': '', 'hash' : '' };
+  if ( sheet.getRange(cell_remote).getValue().toLowerCase() == 'remote' ) {
+    let remote_note = sheet.getRange(cell_remote).getNote();
+    if ( remote_note.length ) {
+      let matches = [...remote_note.matchAll(/(use|url|hash):\s*([\S]+)/gi)];
+      if ( matches.length ) {
+        for ( var i = 0; i < matches.length; i++ ) {
+          remote_options[ matches[i][1].toLowerCase() ] = matches[i][2];
+        }
+      }
+    }
+  }
+
+  let parsed = [];
+  if ( remote_options.use.toLowerCase() == 'true' ) {
+    parsed = parseWorkouts(embedRemoteData(remote_options.url, remote_options.hash));
+  } else if ( sheet.getRange(cell_data).getValue().toLowerCase() == 'data' ) {
+    parsed = parseWorkouts(sheet.getRange(cell_data).getNote());
+  }
+
+  if ( parsed.length ) {
+    renderCSVList(parsed, data_columns);
+    sheet.getRange(cell_udate).setValue('Upd: ' + currentTime());
+    sheet.getRange(cell_udate).setNote('Updated: ' + currentTime('long'));
+  } else {
+    alrt('No usable data source found. Please supply a URL or cell note data.');
+  }
 }
+
+// --  Start: Check Box Trigger -- //
+
+function onEdit(e) {
+  // --- checkbox button actions for non-web/mobile devices --- //
+  let cell_chkbx = 'K1';
+  if ( e.range.getA1Notation() === cell_chkbx && ( e.value === "TRUE" || e.value === "FALSE" ) ) {
+    populateWorkouts();
+  }
+}
+
+// -- End: Check Box Trigger -- //
+
+// -- End: App Start -- //
+
+// === End: Workout Manager Main === //
+
+
+
+
+/*
+
+Data Examples
+
+--- v1 data example ---
+workouts 4x rst:30s
+- 5x pull up, 5 3 2 2
+- db ovh press 2x20lb, 20 12 12 10
+
+--- v2 data example ---
+workout, 1/11 7p, (sw1: pull ups, 1 biceps), (garmin=id_or_url, key=val)
+. 4, pull up, body, 7 4 3 4, 45s
+. 4, hammer, 2x25lb, 16 12 8 10, 30s
+
+*/
 
 let v1 = 'workouts 4x rst:30s\n' +
           '- pull up, 5 3 2 2\n'+ 
@@ -349,7 +457,7 @@ let v2 = 'workout, 1/11 7p, (sw1: pull ups, 1 biceps), (garmin=hey, other=hi)\n'
           '. 4, pull up, body, 7 4 3 4, 45s\n'+
           '. 4, hammer, 2x25lb, 16 12 8 10, 30s';
 
-let main = function() {
+let dataTester = function() {
   console.log(JSON.stringify(workoutDataParser(v1), '', 2));
   console.log(JSON.stringify(workoutDataParser(v2), '', 2));
 }
